2d5ed824ce408f293a39399787c4c817c00bf6de
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-08-30 14:59:20 +0530

    Remove unecessary code

--- Feature_Extract/run_create_rdms.sh
+++
@@ -1,1 +0,0 @@
-python create_RDMs.py -fd './feats/92images_feats/' -sd './rdms/92images_rdms/' -d pearson --net all
--- Feature_Extract/run_generate_features.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python generate_features.py -id '/home/user/Git/VisualBrain/algonautsChallenge2019/Training_Data/92_Image_Set/92images' -sd './feats' --net all
